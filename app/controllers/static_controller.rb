dd210f3d12aba3ae1db172f2f44336e06375207e
mspcity <codepark2019@gmail.com>
2020-08-19 02:32:34 -0400

    which controller do i need to use for pages directions

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,7 @@
 class StaticController < ApplicationController
   
   def home
-    if current_user
-      @user = current_user
-      redirect_to tournament_path(@tournament)
-    end
+    
   end
 
   
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,18 @@
+class UserController < ApplicationController
+  # def new
+  #   @user = User.new
+  # end
+
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     session[:user_id] = @user.user_id
+  #     redirect_to tournaments_path
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # i don't need this because using devise 
+  # but then where do i need put redirect_to tournaments_path
+end
